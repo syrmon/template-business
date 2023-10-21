2792b84563bad7a52ba80c5a97ecd9e8376a64ad
syrmon <mmehemmsd@gmail.com>
2023-10-21 18:47:26 +0400

    added links to the navbar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
 import Services from "./pages/services/Services";
 import AboutUs from "./pages/aboutUs/AboutUs";
+import WebDesign from "./pages/services/webDesign/WebDesign";
+import AppDevelopment from "./pages/services/appDevelopment/AppDevelopment";
+import DigitalMarketing from "./pages/services/digitalMarketing/DigitalMarketing";
 
 const App = () => {
   return (
@@ -12,8 +15,10 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/services" element={<Services />} />
           <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/web-design" element={<WebDesign />} />
+          <Route path="/app-development" element={<AppDevelopment />} />
+          <Route path="/digital-marketing" element={<DigitalMarketing />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,58 @@
 import styles from "./styles.module.css";
 import logoImg from "../../assets/logo.png";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { useNavigate } from "react-router-dom";
 const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
     <div className={`${styles.container} max-width`}>
       <div className={styles.logoContainer}>
         <img src={logoImg} alt="Firm Logo" className={styles.logo} />
       </div>
+
+      <div className={styles.linkContainer}>
+        <span
+          className={`${
+            window.location.pathname === "/" ? styles.selected : styles.link
+          }`}
+          onClick={() => navigate("/")}
+        >
+          Home
+        </span>
+
+        <span
+          className={
+            window.location.pathname === "/web-design"
+              ? styles.selected
+              : styles.link
+          }
+          onClick={() => navigate("/web-design")}
+        >
+          Website Design
+        </span>
+        <span
+          className={
+            window.location.pathname === "/app-development"
+              ? styles.selected
+              : styles.link
+          }
+          onClick={() => navigate("/app-development")}
+        >
+          App Development
+        </span>
+        <span
+          className={
+            window.location.pathname === "/digital-marketing"
+              ? styles.selected
+              : styles.link
+          }
+          onClick={() => navigate("/digital-marketing")}
+        >
+          Digital Marketing
+        </span>
+      </div>
+
       <div className={styles.hamburgerMenuContainer}>
         <GiHamburgerMenu className={styles.hamburgerMenu} />
       </div>
