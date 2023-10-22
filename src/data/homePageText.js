a1e64ad89798fe4ebe3f7daba85d04af6c81e652
syrmon <mmehemmsd@gmail.com>
2023-10-22 18:19:31 +0400

    added new services

--- src/data/homePageText.js
+++ src/data/homePageText.js
@@ -31,4 +31,36 @@ export const services = [
     iconName: "DigiMark",
     link: "/digital-marketing",
   },
+  {
+    title: "Reklamcilik Dersleri",
+    description:
+      "We specialise in web designing and development. Whether you need to create a new website or update the existing.",
+    subServices: ["UI/UX Design", "Web Development", "Web Content writing"],
+    iconName: "Design",
+    link: "/edu/marketing",
+  },
+  {
+    title: "Graphic Dizayn",
+    description:
+      "Star It is ready to go the extra mile to ensure that you requirement is fulfilled. From eCommerce to education.",
+    subServices: [
+      "Android Development",
+      "IOS Development",
+      "Software Development",
+    ],
+    iconName: "AppDev",
+    link: "/edu/graphich-design",
+  },
+  {
+    title: "Web Development",
+    description:
+      "Generate your sales opportunity with digital marketing services from Star IT. Our strategic solutions.",
+    subServices: [
+      "SEO Services",
+      "Link Building Services",
+      "Page Optimization",
+    ],
+    iconName: "DigiMark",
+    link: "/edu/web-development",
+  },
 ];
